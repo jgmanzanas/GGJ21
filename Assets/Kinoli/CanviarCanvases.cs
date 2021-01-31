47d256aa815d144574ff928e7861596592023873
kinoli <kinoli7@gmail.com>
2021-01-31 12:27:23 +0100

    Añadido menú instrucciones y el parallax

---
+++ Assets/Kinoli/CanviarCanvases.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanviarCanvases : MonoBehaviour
+{
+    public Canvas c;
+
+    public void Canvases()
+    {
+        Debug.Log("Instrucciones");
+        c.sortingOrder = 1;
+    }
+}
